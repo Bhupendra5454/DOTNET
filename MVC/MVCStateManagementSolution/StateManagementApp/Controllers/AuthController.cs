ba6f15e7a1baa86bf13a0d8f7dc1252a1a67ad66
Bhupebdra5454 <akshayw2512@gmail.com>
2024-06-10 16:41:15 +0530

    Login Auth

---
+++ MVC/MVCStateManagementSolution/StateManagementApp/Controllers/AuthController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using StateManagementApp.Models;
+
+namespace StateManagementApp.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            Claim theClaim = new Claim()
+            {
+                Email = "",
+                Password = ""
+            };
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(Claim theClaim)
+        {
+            if(theClaim.Email == "akshayw2512@gmail.com" && theClaim.Password=="Bhupendraw")
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return View(theClaim);
+        }
+    }
+}
--- MVC/MVCStateManagementSolution/StateManagementApp/obj/Debug/net8.0/StateManagementApp.AssemblyInfo.cs
+++ MVC/MVCStateManagementSolution/StateManagementApp/obj/Debug/net8.0/StateManagementApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StateManagementApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44077c7483e8af76879ba59ea58a584e4b11492f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be6398b7ce3e982b5c428f2dda815e0256dfc46e")]
 [assembly: System.Reflection.AssemblyProductAttribute("StateManagementApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StateManagementApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
