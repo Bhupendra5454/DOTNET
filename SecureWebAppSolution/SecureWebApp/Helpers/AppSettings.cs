59963a415a61b5a48bda7ac8889e156321735fa6
Bhupebdra5454 <akshayw2512@gmail.com>
2024-06-20 17:43:21 +0530

    SecureWebApp JwtToken

---
+++ SecureWebAppSolution/SecureWebApp/Helpers/AppSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace SecureWebApp.Helpers
+{
+    public class AppSettings
+    {
+        public string Secret {  get; set; } 
+    }
+}
---
+++ SecureWebAppSolution/SecureWebApp/Program.cs
@@ -0,0 +1,24 @@
+using SecureWebApp.Helpers;
+using SecureWebApp.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.Configure<AppSettings>(builder.Configuration.GetSection("AppSettings"));
+builder.Services.AddScoped<IUserService, UserService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseMiddleware<JwtMiddleware>();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
