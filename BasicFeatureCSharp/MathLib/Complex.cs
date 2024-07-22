136b5ffcbe9f6ff3ae2a5b1c292f3ef475d26b6c
Bhupebdra5454 <akshayw2512@gmail.com>
2024-07-22 17:28:11 +0530

    Shadowing

---
+++ BasicFeatureCSharp/MathLib/Complex.cs
@@ -0,0 +1,55 @@
+﻿namespace MathLib
+{
+    public class Complex
+    {
+        public int Real { get; set; }
+        public int Imag { get; set; }   
+        public Complex() {
+            this.Real = 0;
+            this.Imag = 0;
+        }
+
+        public Complex(int real, int imag)
+        {
+            this.Real = real;
+            this.Imag = imag;
+        }
+
+        public static Complex operator +(Complex a, Complex b)
+        {
+            Complex result = new Complex();
+            result.Real = a.Real + b.Real;
+            result.Imag = a.Imag + b.Imag;
+            return result;
+        }
+
+        public static Complex operator -(Complex a, Complex b)
+        {
+            Complex result = new Complex();
+            result.Real = a.Real - b.Real;
+            result.Imag = a.Imag - b.Imag;
+            return result;
+        }
+
+        public static Complex operator *(Complex a, Complex b)
+        {
+            Complex result = new Complex();
+            result.Real = a.Real * b.Real;
+            result.Imag = a.Imag * b.Imag;
+            return result;
+        }
+
+        public static Complex operator /(Complex a, Complex b)
+        {
+            Complex result = new Complex();
+            result.Real = a.Real / b.Real;
+            result.Imag = a.Imag / b.Imag;
+            return result;
+        }
+
+        public override string ToString()
+        {
+            return "Real Num : "+this.Real+"  Imag Num : "+this.Imag;
+        }
+    }
+}
